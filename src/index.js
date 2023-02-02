7838c08cd81c287400c58d604259c5bd660d67f7
Gabo03303 <114263795+Gabo03303@users.noreply.github.com>
2023-02-02 01:49:29 -0500

    Pantallas 7,10,13 y 16

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Pantalla7 from './Pantalla7';
+import './Styles/pantalla10.css';
+import Pantalla10 from './Pantalla10';
+import './Styles/pantalla13.css';
+import Pantalla13 from './Pantalla13';
+import './Styles/pantalla16.css';
+import Pantalla16 from './Pantalla16';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <a/>
+    <Pantalla10 />
   </React.StrictMode>
 );
 
