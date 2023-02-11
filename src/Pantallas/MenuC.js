1eb199601271a85fdd2a1da1a83bc456b35d799b
cmcb15 <cesar.cb1015@gmail.com>
2023-02-11 00:04:27 -0500

    commit 7

--- src/Pantallas/MenuC.js
+++ src/Pantallas/MenuC.js
@@ -21,12 +21,12 @@ function MenuC(){
                         <Link to={"/"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Cerrar Sesion</Link>
                     </li>
 </ul>*/}
-        <nav class="nav justify-content-end" id="cabecera" style={{border: "hidden;"}}>
-            <Link to={"/pantalla3"} classNameName="nav-link active" aria-current="page" style={{border: "hidden;"}}>Pagina Principal</Link>
-            <Link to={"/sorpresa"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Reseña</Link>
-            <Link to={"/pantalla6"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Carrito</Link>
-            <Link to={"/pantalla16"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Ver Pedido</Link>
-            <Link to={"/"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Cerrar Sesion</Link>
+        <nav class="nav justify-content-end" id="cabecera" style={{border: "hidden;",backgroundColor:"black"}}>
+            <Link to={"/pantalla3"} classNameName="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Pagina Principal</Link>
+            <Link to={"/sorpresa"} className="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Reseña</Link>
+            <Link to={"/pantalla6"} className="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Carrito</Link>
+            <Link to={"/pantalla16"} className="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Ver Pedido</Link>
+            <Link to={"/"} className="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Cerrar Sesion</Link>
         </nav>
 
     </div>
--- src/Pantallas/MenuR.js
+++ src/Pantallas/MenuR.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 
 function MenuR(){
     return <div>
-        <nav class="nav justify-content-end" id="cabecera" style={{border: "hidden;"}}>
-            <Link to={"/pantalla10"} classNameName="nav-link active" aria-current="page" style={{border: "hidden;"}}>Platos</Link>
-            <Link to={"/pantalla13"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Lista Pedidos</Link>
-            <Link to={"#"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Estado Pedidos</Link>
-            <Link to={"/pantalla16"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Registrar Pedidos</Link>
-            <Link to={"/pantalla9"} className="nav-link active" aria-current="page" style={{border: "hidden;"}}>Cerrar Sesion</Link>
+        <nav class="nav justify-content-end" id="cabecera" style={{border: "hidden;",backgroundColor:"black"}}>
+            <Link to={"/pantalla10"} classNameName="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Platos</Link>
+            <Link to={"/pantalla13"} className="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Lista Pedidos</Link>
+            <Link to={"#"} className="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Estado Pedidos</Link>
+            <Link to={"/pantalla16"} className="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Registrar Pedidos</Link>
+            <Link to={"/pantalla9"} className="nav-link active" aria-current="page" style={{border: "hidden;",color:"white"}}>Cerrar Sesion</Link>
         </nav>
     </div>
 }
